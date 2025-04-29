75f05a3915682de8d1be252bf6db377ce9de48d4
Irina Rogozhova <irogozhova@york.citycollege.eu>
2025-04-29 16:00:38 +0300

    Set up springboot app and eureka server

---
+++ src/main/java/com/example/encryption_decryption_app/EncryptionDecryptionAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.encryption_decryption_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncryptionDecryptionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncryptionDecryptionAppApplication.class, args);
+	}
+
+}
