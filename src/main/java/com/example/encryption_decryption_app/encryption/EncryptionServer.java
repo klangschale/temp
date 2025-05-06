d5f940b8c31d37b518955d192a5800604567c2e2
Irina Rogozhova <irogozhova@york.citycollege.eu>
2025-05-06 22:11:35 +0300

    Add encryption service

---
+++ src/main/java/com/example/encryption_decryption_app/encryption/EncryptionServer.java
@@ -0,0 +1,14 @@
+package com.example.encryption_decryption_app.encryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EncryptionServer {
+    public static void main(String[] args) {
+        System.setProperty("spring.config.name", "encryption-server");
+        SpringApplication.run(EncryptionServer.class, args);
+    }
+}
