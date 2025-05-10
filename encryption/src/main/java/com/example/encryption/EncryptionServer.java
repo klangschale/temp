d6a49231e60ee1c1b3a842cdfb8deaaeb2eec90b
Irina Rogozhova <irogozhova@york.citycollege.eu>
2025-05-10 22:09:26 +0300

    Refactor to multi-module project

--- encryption/src/main/java/com/example/encryption/EncryptionServer.java
+++ encryption/src/main/java/com/example/encryption/EncryptionServer.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class EncryptionServer {
     public static void main(String[] args) {
-        System.setProperty("spring.config.name", "encryption-server");
         SpringApplication.run(EncryptionServer.class, args);
     }
 }
